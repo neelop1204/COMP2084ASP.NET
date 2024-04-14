0577cd9ea44cb67f0a14522a17f77469be51dd3b
neelop1204 <200556051@student.georgianc.on.ca>
2024-04-14 12:15:15 -0400

    hiiii

--- obj/Debug/net8.0/LabWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/LabWebApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LabWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97b0c745b1f79510b06eef1e402ce1db6331c237")]
 [assembly: System.Reflection.AssemblyProductAttribute("LabWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LabWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
